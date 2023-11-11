66a8bb71784fc150009965162a1a5fcf97fa20f1
TaldoGus <xgustavopereirax@gmail.com>
2023-11-10 21:15:46 -0300

    Update - Created
    definindo rotas pelo app.py e criando paginas testes de nomes provisórios com estruturas testes provisórias

--- app.py
+++ app.py
@@ -3,3 +3,18 @@ app = Flask("name")
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/quem')
+def quem():
+    return render_template('quem.html')
+
+
+@app.route('/dicas')
+def dicas():
+    return render_template('dicas.html')
+
+
+@app.route('/onde')
+def onde():
+    return render_template('onde.html')
+
